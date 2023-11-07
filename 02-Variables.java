e1699aab001dd7ac0381f15a72b67ebe52adca79
Alex Mwangi <alex@stl-horizon.com>
2023-11-07 16:51:07 +0300

    Variables in Java

---
+++ 02-Variables.java
@@ -0,0 +1,15 @@
+class Variables {
+    public static void main(String [] args){
+
+        
+        // System.out.println(4+10); 14
+        // System.out.println(5+10); 15
+
+        // Working with Variables
+        int numberOne = 4;
+        int numberTwo = 10;
+        int sum = numberOne + numberTwo;
+
+        System.out.println("The sum is : "+sum);
+    }
+}

2ce41bc88748f9bf16bc3581bd2c8014bdfcd29e
Alex Mwangi <alex@stl-horizon.com>
2023-11-08 12:16:27 +0300

    Arithmetic Operators in Java

---
+++ 06-Arithmetic-operators.java
@@ -0,0 +1,39 @@
+class Arithmetic_operators {
+    public static void main(String[] args) {
+       
+        /*
+         *  Arithmetic Operators
+         *  Arithmetic operators are used to perform common mathematical operations.
+
+            Operator	Name	        Description	                                   Example	
+                +	    Addition	    Adds together two values	                    x + y	
+                -	    Subtraction	    Subtracts one value from another	            x - y	
+                *	    Multiplication	Multiplies two values	                        x * y	
+                /	    Division	    Divides one value by another	                x / y	
+                %	    Modulus	        Returns the division remainder	                x % y	
+                ++	    Increment	    Increases the value of a variable by 1	        ++x	
+                --	    Decrement	    Decreases the value of a variable by 1	        --x
+         */
+
+         int firstValue = 200;
+         int secondValue = 110;
+
+         int sum = firstValue + secondValue;
+         int diff = firstValue - secondValue;
+         int product = firstValue * secondValue;
+         float quotient = (float)firstValue / secondValue;
+         int modulo = firstValue % secondValue;
+
+        
+         System.out.println("First Value : "+firstValue );
+         System.out.println("Second Value : "+secondValue );
+
+         System.out.println("------------------------ ");
+         System.out.println("Addition (+) : "+sum);
+         System.out.println("Subtraction (-) : "+diff);
+         System.out.println("Multiplication (*) : "+product);
+         System.out.println("Division (/) : "+quotient);
+         System.out.println("Modulus (%) : "+modulo);
+
+    }
+}

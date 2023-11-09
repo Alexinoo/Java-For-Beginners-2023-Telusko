04087c8608468f6d2cc7e83eddf51c693280807c
Alex Mwangi <alex@stl-horizon.com>
2023-11-09 17:26:30 +0300

    Why we need loops in Java

---
+++ 14-Why-loops.java
@@ -0,0 +1,25 @@
+class Why_loops {
+
+    public static void main(String[] args) {
+
+        // Repeat this statement 15 times
+        // Solution : Loop [ while , do while , for loop ]
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+    }
+    
+}

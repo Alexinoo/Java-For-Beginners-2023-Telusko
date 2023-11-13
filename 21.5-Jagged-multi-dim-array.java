d506055b9e3645e2ef60245df53c6f1a85c6c24e
Alex Mwangi <alex@stl-horizon.com>
2023-11-13 15:00:06 +0300

    Jagged Array in Java

---
+++ 21.5-Jagged-multi-dim-array.java
@@ -0,0 +1,50 @@
+ /*  Jagged Multi-dimensional Array 
+     * 
+     * Jagged - Stores several arrays of varying sizes
+     * 
+     * Jagged - numbers[][] = new int[3][] -- 3 arrays but not sure of the internal elements
+     *  
+     *  
+     * Assigning Sizes 
+     * 
+     * numbers[0] = new int[3] ;  1st Array - will have 3 elements
+     * numbers[1] = new int[4] ;  2nd Array - will have 4 elements
+     * numbers[2] = new int[2] ;  3rd Array - will have 2 elements
+     */
+
+class Jagged_multi_dim_array {
+    public static void main(String[] args) {
+        
+        int numbers[][] = new int[3][]; //jagged
+
+        numbers[0] = new int[3];
+        numbers[1] = new int[4];
+        numbers[2] = new int[2];
+
+        //Normal  Loop
+        
+        System.out.println("==== Normal For Loop ============");
+
+        for (int i = 0; i < numbers.length; i++) {        
+
+            for (int j = 0; j < numbers[i].length; j++) {
+
+                numbers[i][j] = (int)(Math.random()*10);
+
+                System.out.print(numbers[i][j] +" ");
+              }
+               
+            System.out.println();
+        }
+
+
+        System.out.println("==== For Each Loop ============");
+
+        for (int i[] : numbers) {
+            for (int j : i) {
+                System.out.print(j +" ");
+            }
+            System.out.println();
+        }
+    }
+}

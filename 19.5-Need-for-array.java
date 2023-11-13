15f594787a2638c2f17f2dd5db0dbea9fbb674c4
Alex Mwangi <alex@stl-horizon.com>
2023-11-13 13:10:15 +0300

    Need for Arrays in Java

---
+++ 19.5-Need-for-array.java
@@ -0,0 +1,18 @@
+class Need_for_array {
+
+    public static void main(String[] args) {
+        
+        // individual variables [ Remember the names ]
+         int i = 5;
+         int j = 6;
+         int k = 7;
+
+        //  Store several values in 1 collection [1  stores multiple values]
+        int numbers[] = {5, 6, 7};
+
+
+        // Set Sized Array to store 4 elements - Define size of the array in advance
+        int num[] = new int[4];
+    }
+    
+}
